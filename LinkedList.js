c5fce3cd8829b65176edefb19960de798c384900
mashigovincent01 <mashigovincent407@gmail.com>
2024-09-24 12:46:49 +0200

    update readme and add comments

--- LinkedList.js
+++ LinkedList.js
@@ -1,215 +1,227 @@
+// Node class representing a single node in the linked list
 class Node {
     constructor(value = null, next = null) {
-        this.value = value;
-        this.next = next;
+        this.value = value; // Value stored in the node
+        this.next = next;   // Pointer to the next node
     }
 }
 
+// LinkedList class representing the entire linked list
 class LinkedList {
     constructor() {
-        this._head = null;
-        this._size = 0;
-        this._tail = null;
+        this._head = null;  // Head of the list
+        this._size = 0;     // Number of elements in the list
+        this._tail = null;  // Tail of the list
     }
 
+    // Getter for the head of the list
     get head() {
         return this._head;
     }
 
+    // Getter for the size of the list
     get size() {
         return this._size;
     }
 
+    // Getter for the tail of the list
     get tail() {
         return this._tail;
     }
 
+    // Prepend a new node with the given value to the start of the list
     prepend(value) {
         if (this._head === null) {
-            this._head = new Node(value);
+            this._head = new Node(value); // Create a new head if the list is empty
             this._tail = this._head; // Set tail to head if list was empty
         } else {
-            this._head = new Node(value, this._head);
+            this._head = new Node(value, this._head); // Create a new node and point to the old head
         }
-        this._size++;
+        this._size++; // Increase the size of the list
     }
 
+    // Append a new node with the given value to the end of the list
     append(value) {
         if (this._head === null) {
-            this._head = new Node(value);
+            this._head = new Node(value); // Create a new head if the list is empty
             this._tail = this._head; // Set tail to the first node
         } else {
-            this._tail.next = new Node(value);
-            this._tail = this._tail.next; // Move the tail
+            this._tail.next = new Node(value); // Link new node to the end
+            this._tail = this._tail.next; // Move the tail pointer to the new node
         }
-        this._size++;
+        this._size++; // Increase the size of the list
     }
 
+    // Convert the linked list to a string representation
     toString() {
-        return this.toStringRec(this._head) + "null";
+        return this.toStringRec(this._head) + "null"; // Call recursive helper and append null
     }
 
+    // Helper method for toString to recursively traverse the list
     toStringRec(head, res = "") {
         if (head === null) {
-            return res;
+            return res; // Return the accumulated string if end of the list is reached
         }
-        return res + "( " + head.value + " ) " + "-> " + this.toStringRec(head.next);
+        return res + "( " + head.value + " ) " + "-> " + this.toStringRec(head.next); // Concatenate value and recurse
     }
-    at(index){
-        if(this._head === null || index < 0 || index >= this.size){
-            return undefined;
-        }
-        else{
+
+    // Get the node at a specific index
+    at(index) {
+        if (this._head === null || index < 0 || index >= this.size) {
+            return undefined; // Return undefined for out-of-bounds index
+        } else {
             let count = 0;
-            let temp = this.head;
-            while(temp !== null){
-                if(count === index){
-                    return temp;
+            let temp = this.head; // Start at the head
+            while (temp !== null) { // Traverse until the end
+                if (count === index) {
+                    return temp; // Return the node at the given index
                 }
-                temp = temp.next;
+                temp = temp.next; // Move to the next node
                 count++;
             }
         }
     }
 
+    // Remove and return the last node of the list
     pop() {
         // Case 1: If the list is empty
         if (this._head === null) {
             return null; // Nothing to pop
         }
-    
+
         // Case 2: If the list has only one element
         if (this._head.next === null) {
-            let poppedValue = this._head.value;
-            this._head = null;
-            this._tail = null;
-            this._size--;
-            return poppedValue;
+            let poppedValue = this._head.value; // Store the value to return
+            this._head = null; // Clear the head
+            this._tail = null; // Clear the tail
+            this._size--; // Decrease the size
+            return poppedValue; // Return the value of the popped node
         }
-    
+
         // Case 3: Traverse to the second-to-last node
         let current = this._head;
         while (current.next !== this._tail) {
-            current = current.next;
+            current = current.next; // Move to the second-to-last node
         }
-    
+
         // At this point, current is the second-to-last node
-        let poppedValue = this._tail.value;
-        current.next = null;
-        this._tail = current;
-        this._size--;
-    
+        let poppedValue = this._tail.value; // Store the value of the tail
+        current.next = null; // Remove the last node
+        this._tail = current; // Update the tail to the current node
+        this._size--; // Decrease the size
+
         return poppedValue; // Return the value of the popped node
     }
 
-    contains(value){
-        if(this._head === null){
-            return false;
+    // Check if the list contains a node with the given value
+    contains(value) {
+        if (this._head === null) {
+            return false; // Return false if the list is empty
         }
         let res = false;
-        let temp = this._head;
-        while(temp !== null){
-            if(temp.value === value){
-                return true;
+        let temp = this._head; // Start at the head
+        while (temp !== null) { // Traverse until the end
+            if (temp.value === value) {
+                return true; // Return true if the value is found
             }
-            temp = temp.next;
+            temp = temp.next; // Move to the next node
         }
-        return res;
+        return res; // Return false if the value is not found
     }
 
-    find(value){
-        if(this._head === null){
-            return null;
+    // Find the index of the first node with the given value
+    find(value) {
+        if (this._head === null) {
+            return null; // Return null if the list is empty
         }
         let index = 0;
-        let temp = this._head;
-        while(temp !== null){
-            if(temp.value === value){
-                return index;
+        let temp = this._head; // Start at the head
+        while (temp !== null) { // Traverse until the end
+            if (temp.value === value) {
+                return index; // Return the index if the value is found
             }
-            temp = temp.next;
+            temp = temp.next; // Move to the next node
             index++;
         }
-        return null;
+        return null; // Return null if the value is not found
     }
+
+    // Insert a new node with the given value at the specified index
     insertAt(value, index) {
         if (index < 0 || index > this._size) {
-            throw new Error("Index out of bounds");
+            throw new Error("Index out of bounds"); // Throw error for invalid index
         }
 
         // Case 1: Insert at the beginning
         if (index === 0) {
-            this.prepend(value);
+            this.prepend(value); // Use prepend method
             return;
         }
 
         // Case 2: Insert at the end
         if (index === this._size) {
-            this.append(value);
+            this.append(value); // Use append method
             return;
         }
 
         // Case 3: Insert in the middle
-        let newNode = new Node(value);
-        let current = this._head;
-        let previous = null;
+        let newNode = new Node(value); // Create a new node
+        let current = this._head; // Start at the head
+        let previous = null; // Previous node
         let currentIndex = 0;
 
-        while (currentIndex < index) {
-            previous = current;
-            current = current.next;
+        while (currentIndex < index) { // Traverse to the desired index
+            previous = current; // Store the previous node
+            current = current.next; // Move to the current node
             currentIndex++;
         }
 
-        previous.next = newNode;
-        newNode.next = current;
-        this._size++;
+        previous.next = newNode; // Link the new node
+        newNode.next = current; // Link the current node
+        this._size++; // Increase the size of the list
     }
 
     // Remove a node at the specified index
     removeAt(index) {
         if (index < 0 || index >= this._size) {
-            throw new Error("Index out of bounds");
+            throw new Error("Index out of bounds"); // Throw error for invalid index
         }
 
         // Case 1: Remove the head node
         if (index === 0) {
-            let removedValue = this._head.value;
-            this._head = this._head.next;
+            let removedValue = this._head.value; // Store the value to return
+            this._head = this._head.next; // Move head to the next node
             if (this._head === null) {
                 this._tail = null; // If list becomes empty
             }
-            this._size--;
-            return removedValue;
+            this._size--; // Decrease the size
+            return removedValue; // Return the value of the removed node
         }
 
-        let current = this._head;
-        let previous = null;
+        let current = this._head; // Start at the head
+        let previous = null; // Previous node
         let currentIndex = 0;
 
         // Traverse to the node just before the target node
         while (currentIndex < index) {
-            previous = current;
-            current = current.next;
+            previous = current; // Store the previous node
+            current = current.next; // Move to the current node
             currentIndex++;
         }
 
         // Case 2: Remove from the middle or end
-        let removedValue = current.value;
-        previous.next = current.next;
+        let removedValue = current.value; // Store the value of the node to be removed
+        previous.next = current.next; // Link previous node to the next node
 
         // If we are removing the last node, update the tail
         if (index === this._size - 1) {
-            this._tail = previous;
+            this._tail = previous; // Update tail to the previous node
         }
 
-        this._size--;
-        return removedValue;
+        this._size--; // Decrease the size
+        return removedValue; // Return the value of the removed node
     }
-    
 }
 
+// Export the LinkedList class for external use
 module.exports = LinkedList;
-
-
-
--- README.md
+++ README.md
@@ -1,1 +1,120 @@
-# TheOrdingProject_08_LinkedList
+# LinkedList Implementation
+
+This project implements a simple **Linked List** data structure in JavaScript. The `LinkedList` class provides methods for common operations such as appending, prepending, finding, removing, and accessing nodes by index.
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Methods](#methods)
+  - [prepend(value)](#prependvalue)
+  - [append(value)](#appendvalue)
+  - [toString()](#tostring)
+  - [at(index)](#atindex)
+  - [pop()](#pop)
+  - [contains(value)](#containsvalue)
+  - [find(value)](#findvalue)
+  - [insertAt(value, index)](#insertatvalue-index)
+  - [removeAt(index)](#removeatindex)
+- [Example](#example)
+
+
+## Installation
+
+1. Clone the repository or download the source code.
+2. Ensure you have **Node.js** installed on your machine.
+3. Run the following command in your terminal to initialize the project:
+
+```bash
+   npm install
+```
+
+## Usage
+
+Import the LinkedList class into your project:
+```Javascript
+const LinkedList = require('./LinkedList');
+```
+
+Create and interact with the LinkedList:
+
+```javascript
+const list = new LinkedList(); list.append(10);
+ list.prepend(5); 
+ console.log(list.toString()); // ( 5 ) -> ( 10 ) -> null
+```
+## Methods
+### prepend(value)
+Adds a new node with the specified value to the beginning of the list.
+If the list is empty, the new node becomes both the head and the tail.
+```javascript
+list.prepend(5);
+```
+
+### append(value)
+Adds a new node with the specified value to the end of the list.
+If the list is empty, the new node becomes both the head and the tail.
+```javascript
+list.append(10);
+```
+
+### toString()
+Returns a string representation of the list, showing all nodes and their values in order.
+```javascript
+console.log(list.toString()); // ( 5 ) -> ( 10 ) -> null
+```
+
+### at(index)
+Returns the node at the specified index, or undefined if the index is out of bounds.
+```javascript
+console.log(list.at(1)); // Node with value 10
+```
+
+### pop()
+Removes and returns the last node in the list.
+If the list is empty, it returns null.
+```javascript
+console.log(list.pop()); // 10
+```
+### contains(value)
+Returns true if the list contains a node with the specified value, otherwise returns false.
+```javascript
+console.log(list.contains(5)); // true
+```
+
+### find(value)
+Returns the index of the node containing the specified value, or null if the value is not found.
+```javascript
+console.log(list.find(10)); // 1
+```
+
+### insertAt(value, index)
+Inserts a new node with the specified value at the given index.
+Throws an error if the index is out of bounds.
+```javascript
+list.insertAt(7, 1); // Inserts value 7 at index 1
+```
+
+### removeAt(index)
+Removes the node at the specified index and returns its value.
+Throws an error if the index is out of bounds.
+```javascript
+list.removeAt(0); // Removes the head node
+```
+
+## Example
+```javascript
+const list = new LinkedList(); 
+list.append(10); 
+list.prepend(5); 
+list.insertAt(7, 1); 
+console.log(list.toString()); // ( 5 ) -> ( 7 ) -> ( 10 ) -> null
+
+console.log(list.contains(7)); // true console.log(list.find(10)); // 2
+
+list.pop(); 
+console.log(list.toString()); // ( 5 ) -> ( 7 ) -> null
+
+list.removeAt(1);
+console.log(list.toString()); // ( 5 ) -> null
+```
